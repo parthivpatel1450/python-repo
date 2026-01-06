9ba3db3378e845a6c0efb26dd5190bdfad4f7e75
Parthiv Patel <patel.parthiv145@gmail.com>
2026-01-06 16:43:05 +0530

    changes in 10.py file python

---
+++ python training/10.py
@@ -0,0 +1,7 @@
+print ("Both operands are integer")
+a=5
+b=7
+print ("a=",a, "b=",b, "a>b is", a>b)
+print ("a=",a, "b=",b,"a<b is",a<b)
+print ("a=",a, "b=",b,"a==b is",a==b)
+print ("a=",a, "b=",b,"a!=b is",a!=b)
