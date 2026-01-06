cb7e2b9186c23f44a2603717e9040f02b66db5f3
Parthiv Patel <patel.parthiv145@gmail.com>
2026-01-06 11:22:34 +0530

    Add 4.py

---
+++ python training/4.py
@@ -0,0 +1,11 @@
+# integer variable.
+a=100
+print("The type of variable having value", a, " is ", type(a))
+
+# float variable.
+c=20.345
+print("The type of variable having value", c, " is ", type(c))
+
+# complex variable.
+d=10+3j
+print("The type of variable having value", d, " is ", type(d))
