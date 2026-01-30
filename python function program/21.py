025e8bd24f6f6ea1ea6fac7b2ae1e61ed04995b6
Parthiv Patel <patel.parthiv145@gmail.com>
2026-01-30 17:17:33 +0530

    add files

---
+++ python function program/21.py
@@ -0,0 +1,5 @@
+def pack_kwargs(**kwargs):
+    print("Packed keyword arguments:", kwargs)
+
+# Calling the function with multiple keyword arguments
+pack_kwargs(name="Alice", age=30, city="New York")
